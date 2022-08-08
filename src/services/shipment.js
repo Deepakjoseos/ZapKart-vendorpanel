4a743bb47fac53fdc0773560aa19f0836e3286c8
Afras <muhammedafras0@gmail.com>
2022-08-08 09:43:21 +0530

    shipments

--- src/services/shipment.js
+++ src/services/shipment.js
@@ -43,7 +43,7 @@ shipmentService.getShipmentById = async function (id) {
 shipmentService.createShipment = async function (data) {
   try {
     const res = await fetch({
-      url: apiRoute,
+      url: `${apiRoute}/create`,
       method: 'post',
       data: data,
     })
--- src/views/app-views/dashboards/shipment/ShipmentForm/index.js
+++ src/views/app-views/dashboards/shipment/ShipmentForm/index.js
@@ -3,140 +3,114 @@ import PageHeaderAlt from 'components/layout-components/PageHeaderAlt'
 import { Tabs, Form, Button, message } from 'antd'
 import Flex from 'components/shared-components/Flex'
 import GeneralField from './GeneralField'
-import useUpload from 'hooks/useUpload'
-import { singleImageUploader } from 'utils/s3/s3ImageUploader'
-import brandService from 'services/brand'
-import Utils from 'utils'
+
+import shipmentService from 'services/shipment'
 import { useHistory } from 'react-router-dom'
+import Shipment from '..'
+import moment from 'moment'
 
 const { TabPane } = Tabs
 
 const ADD = 'ADD'
 const EDIT = 'EDIT'
 
-const ProductForm = (props) => {
+const ShipmentForm = (props) => {
   const { mode = ADD, param } = props
   const history = useHistory()
 
   const [form] = Form.useForm()
 
-  // For Image Upload
-  const [uploadedImg, setImage] = useState(null)
   const [submitLoading, setSubmitLoading] = useState(false)
 
-  // For Image upload
-  const {
-    fileList: fileListImages,
-    beforeUpload: beforeUploadImages,
-    onChange: onChangeImages,
-    onRemove: onRemoveImages,
-    setFileList: setFileListImages,
-  } = useUpload(1) // useUpload(1, 'multiple') or useUpload(1)
+  // useEffect(() => {
+  //   if (mode === EDIT) {
+  //     const fetchBrandById = async () => {
+  //       const { id } = param
+  //       const data = await shipmentService.getBrandById(id)
+  //       if (data) {
+  //         // For Image upload
+  //         let himg = []
+  //         if (data.image) {
+  //           himg = [
+  //             {
+  //               uid: Math.random() * 1000,
+  //               name: Utils.getBaseName(data.image),
+  //               url: data.image,
+  //               thumbUrl: data.image,
+  //             },
+  //           ]
 
-  useEffect(() => {
-    if (mode === EDIT) {
-      const fetchBrandById = async () => {
-        const { id } = param
-        const data = await brandService.getBrandById(id)
-        if (data) {
-          // For Image upload
-          let himg = []
-          if (data.image) {
-            himg = [
-              {
-                uid: Math.random() * 1000,
-                name: Utils.getBaseName(data.image),
-                url: data.image,
-                thumbUrl: data.image,
-              },
-            ]
+  //           setImage(himg)
+  //           setFileListImages(himg)
+  //         }
+  //         // For setting form values when Load if it is in EDIT mode
+  //         form.setFieldsValue({
+  //           name: data.name,
+  //           status: data.status,
+  //           priority: data.priority,
+  //         })
+  //       } else {
+  //         history.replace('/app/dashboards/catalog/brand/brands-list')
+  //       }
+  //     }
 
-            setImage(himg)
-            setFileListImages(himg)
-          }
-          // For setting form values when Load if it is in EDIT mode
-          form.setFieldsValue({
-            name: data.name,
-            status: data.status,
-            priority: data.priority,
-          })
-        } else {
-          history.replace('/app/dashboards/catalog/brand/brands-list')
-        }
-      }
-
-      fetchBrandById()
-    }
-  }, [form, mode, param, props])
-
-  // Image Upload
-  const propsImages = {
-    multiple: false,
-    beforeUpload: beforeUploadImages,
-    onRemove: onRemoveImages,
-    onChange: onChangeImages,
-    fileList: fileListImages,
-  }
-
-  // Image Upload
-  useEffect(() => {
-    setImage(fileListImages)
-  }, [fileListImages])
+  //     fetchBrandById()
+  //   }
+  // }, [form, mode, param, props])
 
   // Trigger When Submit Button pressed
   const onFinish = async () => {
     setSubmitLoading(true)
+
     form
       .validateFields()
       .then(async (values) => {
+        const sendingValues = {
+          shippedByVendor: values.shippedByVendor,
+          items: values.items,
+          expectedDeliveryDate: moment(values.expectedDeliveryDate).format(
+            'YYYY-MM-DD'
+          ),
+          shipRocket: {
+            pickup_location: values.pickup_location,
+            length: values.length,
+            breadth: values.breadth,
+            height: values.height,
+            weight: values.weight,
+          },
+        }
+
         if (mode === ADD) {
-          // Checking if image exists
-          if (uploadedImg.length !== 0 && uploadedImg !== null) {
-            console.log('uploadedImg', uploadedImg)
-            // We will upload image to S3 and get the image url
-            const imgValue = await singleImageUploader(
-              uploadedImg[0].originFileObj,
-              uploadedImg,
-              uploadedImg[0].url,
-              'brand'
-            )
-
-            //  append image url to values object
-            values.image = imgValue
-
-            const created = await brandService.createBrand(values)
-            if (created) {
-              message.success(`Created ${values.name} to Brand list`)
-              history.goBack()
-            }
-          } else {
-            message.error('Please upload image')
+          const created = await shipmentService.createShipment(sendingValues)
+          if (created) {
+            message.success(`Created ${values.name} to Shipment list`)
+            history.goBack()
           }
         }
-        if (mode === EDIT) {
-          // Checking if image exists
-          if (uploadedImg.length !== 0 && uploadedImg !== null) {
-            console.log('uploadedImg', uploadedImg)
-            // We will upload image to S3 and get the image url
-            const imgValue = await singleImageUploader(
-              uploadedImg[0].originFileObj,
-              uploadedImg,
-              uploadedImg[0].url,
-              'brand'
-            )
+        // if (mode === EDIT) {
+        //   // Checking if image exists
+        //   if (uploadedImg.length !== 0 && uploadedImg !== null) {
+        //     console.log('uploadedImg', uploadedImg)
+        //     // We will upload image to S3 and get the image url
+        //     const imgValue = await singleImageUploader(
+        //       uploadedImg[0].originFileObj,
+        //       uploadedImg,
+        //       uploadedImg[0].url,
+        //       'brand'
+        //     )
 
-            //  append image url to values object
-            values.image = imgValue
+        //     //  append image url to values object
+        //     values.image = imgValue
 
-            const edited = await brandService.editBrand(param.id, values)
-            if (edited) {
-              message.success(`Edited ${values.name} to Brand list`)
-              history.goBack()
-            }
-          } else {
-            message.error('Please upload image')
-          }
-        }
+        //     const edited = await brandService.editBrand(param.id, values)
+        //     if (edited) {
+        //       message.success(`Edited ${values.name} to Brand list`)
+        //       history.goBack()
+        //     }
+        //   } else {
+        //     message.error('Please upload image')
+        //   }
+        // }
         setSubmitLoading(false)
       })
       .catch((info) => {
@@ -155,6 +129,12 @@ const ProductForm = (props) => {
         className="ant-advanced-search-form"
         initialValues={{
           status: 'Hold',
+          items: [
+            {
+              orderId: '',
+              itemIds: [],
+            },
+          ],
         }}
       >
         <PageHeaderAlt className="border-bottom" overlap>
@@ -166,13 +146,13 @@ const ProductForm = (props) => {
               alignItems="center"
             >
               <h2 className="mb-3">
-                {mode === 'ADD' ? 'Add New Brand' : `Edit Brand`}{' '}
+                {mode === 'ADD' ? 'Add New Shipment' : `Edit Shipment`}{' '}
               </h2>
               <div className="mb-3">
                 <Button
                   className="mr-2"
                   onClick={() =>
-                    history.push('/app/dashboards/catalog/brand/brands-list')
+                    history.push('/app/dashboards/shipment/shipment-list')
                   }
                 >
                   Discard
@@ -192,12 +172,7 @@ const ProductForm = (props) => {
         <div className="container">
           <Tabs defaultActiveKey="1" style={{ marginTop: 30 }}>
             <TabPane tab="General" key="1">
-              <GeneralField
-                uploadedImg={uploadedImg}
-                // uploadLoading={uploadLoading}
-                // handleUploadChange={handleUploadChange}
-                propsImages={propsImages}
-              />
+              <GeneralField form={form} />
             </TabPane>
           </Tabs>
         </div>
@@ -206,4 +181,4 @@ const ProductForm = (props) => {
   )
 }
 
-export default ProductForm
+export default ShipmentForm
