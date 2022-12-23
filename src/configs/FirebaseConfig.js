5b54295fe2b5c2ebeb2560ca287314bcb9733642
Afras <muhammedafras0@gmail.com>
2022-12-23 09:00:17 +0530

    changes

--- src/configs/FirebaseConfig.js
+++ src/configs/FirebaseConfig.js
@@ -148,11 +148,11 @@ if (process.env.REACT_APP_SITE_NAME === 'athathy') {
     // messagingSenderId: '295619267638',
     // appId: '1:295619267638:web:5bb8523b198a33959bdc97',
     apiKey: 'AIzaSyDE96sBpGy5JJKHNwTHP3OuUSChRCIgH_U',
-  authDomain: 'ecommerce-74b71.firebaseapp.com',
-  projectId: 'ecommerce-74b71',
-  storageBucket: 'ecommerce-74b71.appspot.com',
-  messagingSenderId: '1031062435017',
-  appId: '1:1031062435017:web:d6b5ac03cd40d344480918'
+    authDomain: 'ecommerce-74b71.firebaseapp.com',
+    projectId: 'ecommerce-74b71',
+    storageBucket: 'ecommerce-74b71.appspot.com',
+    messagingSenderId: '1031062435017',
+    appId: '1:1031062435017:web:d6b5ac03cd40d344480918',
   }
 } else {
   switch (process.env.NODE_ENV) {
@@ -164,13 +164,13 @@ if (process.env.REACT_APP_SITE_NAME === 'athathy') {
         // storageBucket: 'zapkartnew2022-dc8a8.appspot.com',
         // messagingSenderId: '812753080859',
         // appId: '1:812753080859:web:63d1a2c52ff2a2a5161ebe',
-       
-        apiKey: 'AIzaSyDVRszGmdIg-Me-79_x9yuntOdt6Mz2LdM',
-        authDomain: 'zapkartlive.firebaseapp.com',
-        projectId: 'zapkartlive',
-        storageBucket: 'zapkartlive.appspot.com',
-        messagingSenderId: '1010821971171',
-        appId: '1:1010821971171:web:dcd6a429a1f65cb3ead9d6',
+
+        apiKey: 'AIzaSyDE96sBpGy5JJKHNwTHP3OuUSChRCIgH_U',
+        authDomain: 'ecommerce-74b71.firebaseapp.com',
+        projectId: 'ecommerce-74b71',
+        storageBucket: 'ecommerce-74b71.appspot.com',
+        messagingSenderId: '1031062435017',
+        appId: '1:1031062435017:web:d6b5ac03cd40d344480918',
       }
       break
     case 'production':
@@ -185,12 +185,12 @@ if (process.env.REACT_APP_SITE_NAME === 'athathy') {
         }
       } else {
         FirebaseConfig = {
-          apiKey: 'AIzaSyDE96sBpGy5JJKHNwTHP3OuUSChRCIgH_U',
-          authDomain: 'ecommerce-74b71.firebaseapp.com',
-          projectId: 'ecommerce-74b71',
-          storageBucket: 'ecommerce-74b71.appspot.com',
-          messagingSenderId: '1031062435017',
-          appId: '1:1031062435017:web:d6b5ac03cd40d344480918',
+          apiKey: 'AIzaSyDVRszGmdIg-Me-79_x9yuntOdt6Mz2LdM',
+          authDomain: 'zapkartlive.firebaseapp.com',
+          projectId: 'zapkartlive',
+          storageBucket: 'zapkartlive.appspot.com',
+          messagingSenderId: '1010821971171',
+          appId: '1:1010821971171:web:dcd6a429a1f65cb3ead9d6',
         }
       }
       break
